102338116cea60514bc3b377fa1a29ebc20e4255
Christian Felipe <cfelipe@qdqmedia.com>
2015-01-22 16:33:10 +0100

    basic package structure

--- django_tastypie_digester/__init__.py
+++ django_tastypie_digester/__init__.py
@@ -4,6 +4,4 @@ Client for TastyPie based services (other REST services should work too)
 
 from .core import Api
 
-__version__ = '0.1.0'
-
 __all__ = ['Api']
---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="django_tastypie_digester",
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['requests>=2.3.0']
+)
